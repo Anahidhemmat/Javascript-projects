b8fb706ce95f1c8f116298c88037bc01918d08d2
Anahid <anahidhemmat2130@gmail.com>
2022-02-07 13:04:21 +0400

    Done

--- Quiz App/app.js
+++ Quiz App/app.js
@@ -1,11 +1,11 @@
 const quizData = [
   {
-    question: "How old is Anahid?",
-    a: "10",
-    b: "17",
-    c: "22",
-    d: "24",
-    correct: "b",
+    question: "Which language runs in a web browser?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "Javacript",
+    correct: "d ",
   },
   {
     question: "What is the most used programming language?",
@@ -16,12 +16,12 @@ const quizData = [
     correct: "d",
   },
   {
-    question: "Who is the president of US?",
-    a: "Florin",
-    b: "Trump",
-    c: "Biden",
-    d: "Somayeh",
-    correct: "c",
+    question: "What does css stand for?",
+    a: "Central style sheets",
+    b: "Cascading style sheets",
+    c: "Cascading simple sheets",
+    d: "Care SUVs Sailboats",
+    correct: "b",
   },
   {
     question: "What dose the HTML stand for?",
@@ -37,40 +37,48 @@ const quizData = [
     b: "1995",
     c: "1994",
     d: "none of the above",
-    correct: "d",
+    correct: "b",
   },
 ];
 
 //selectors
 
+const quiz = document.querySelector("#quiz");
+const answerEls = document.querySelectorAll(".answer");
 const questionEl = document.querySelector("#question");
 const aLabel = document.querySelector("#a-label");
 const bLabel = document.querySelector("#b-label");
 const cLabel = document.querySelector("#c-label");
 const dLabel = document.querySelector("#d-label");
-const submitBtn = document.querySelector("button");
+const submitBtn = document.querySelector("#submit");
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 //function for everytime we submit
 
 function loadQuiz() {
+  deselectAnswers();
+
   const currentQuizData = quizData[currentQuiz];
+
   questionEl.innerText = currentQuizData.question;
   aLabel.innerText = currentQuizData.a;
   bLabel.innerText = currentQuizData.b;
   cLabel.innerText = currentQuizData.c;
   dLabel.innerText = currentQuizData.d;
-  currentQuiz++;
+}
+
+//deselect answer
+function deselectAnswers() {
+  answerEls.forEach((answerEl) => (answerEl.checked = false));
 }
 //get selected one
 
 function getSelected() {
-  const answerEls = document.querySelectorAll(".answer");
-
-  let answer = undefined;
+  let answer;
   answerEls.forEach((answerEl) => {
     if (answerEl.checked) {
       answer = answerEl.id;
@@ -85,12 +93,26 @@ submitBtn.addEventListener("click", () => {
   let answer = getSelected();
 
   if (answer) {
+    if (answer === quizData[currentQuiz].correct) {
+      score++;
+    }
     currentQuiz++;
     if (currentQuiz < quizData.length) {
       loadQuiz();
     } else {
       //show results
-      alert("you are done");
+      quiz.innerText = `You answered correctly at ${score}/${quizData.length} questions correctly.`;
+      quiz.style.fontSize = "1.5rem";
+      quiz.style.fontWeight = "bolder";
+      quiz.style.textAlign = "center";
+      const reloadBtn = document.createElement("button");
+      reloadBtn.innerText = "Reload";
+      quiz.insertAdjacentElement("beforeend", reloadBtn);
+      reloadBtn.addEventListener("click", () => {
+        location.reload();
+      });
     }
   }
 });
+
+console.log(quizData.length);
