15504ad1d20edd2fbe670a3b622753e07bcb81c5
Anahid <anahidhemmat2130@gmail.com>
2022-02-07 23:00:23 +0400

    selectors

--- Notes App/app.js
+++ Notes App/app.js
@@ -2,3 +2,5 @@
 const notesEl = document.querySelector(".notes");
 const editBtn = document.querySelector(".edit");
 const deleteBtn = document.querySelector(".delete");
+const mainEl = document.querySelector(".main");
+const textArea = document.querySelector("textarea");
