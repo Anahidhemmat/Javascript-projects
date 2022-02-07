42658f11f111025021da7f5a7c8516b701d6842d
Anahid <anahidhemmat2130@gmail.com>
2022-02-07 23:07:17 +0400

    edit button

--- Notes App/app.js
+++ Notes App/app.js
@@ -4,3 +4,10 @@ const editBtn = document.querySelector(".edit");
 const deleteBtn = document.querySelector(".delete");
 const mainEl = document.querySelector(".main");
 const textArea = document.querySelector("textarea");
+
+//edit button
+
+editBtn.addEventListener("click", () => {
+  mainEl.classList.toggle("hidden");
+  textArea.classList.toggle("hidden");
+});
