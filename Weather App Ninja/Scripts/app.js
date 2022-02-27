31facb3e9f6e9331f09db0588b08e5ebe6359bff
Anahid <anahidhemmat2130@gmail.com>
2022-02-27 19:30:23 +0400

    update ui => function

--- Weather App Ninja/Scripts/app.js
+++ Weather App Ninja/Scripts/app.js
@@ -1,8 +1,28 @@
 //selectors
 const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const cityName = document.querySelector(".details h5");
+const weCon = document.querySelector(".we-con");
+const tempSpan = document.querySelector(".temp");
 
+//update ui => function
+const updateUi = (data) => {
+  const cityDets = data.cityDetail;
+  const weather = data.weather;
+
+  //display data
+  cityName.textContent = cityDets.EnglishName;
+  weCon.textContent = weather.WeatherText;
+  tempSpan.textContent = weather.Temperature.Metric.Value;
+  console.log(data);
+
+  //remove d-none class from a card
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
 //update next city => function
-
 const updateCity = async (city) => {
   const cityDetail = await getCity(city);
   const weather = await getWeather(cityDetail.Key);
@@ -23,6 +43,6 @@ cityForm.addEventListener("submit", (e) => {
 
   //update the ui with new city
   updateCity(city)
-    .then((data) => console.log(data))
+    .then((data) => updateUi(data))
     .catch((err) => console.log(err));
 });
