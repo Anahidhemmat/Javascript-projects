e64a1a6bba2ea89e27ddffa58b1f8cbac5b0980e
Anahid <anahidhemmat2130@gmail.com>
2022-02-27 17:23:43 +0400

    get weather information => function

--- Weather App Ninja/Scripts/forcast.js
+++ Weather App Ninja/Scripts/forcast.js
@@ -1,7 +1,17 @@
 const key = "R0kHJGpfEsVALM8nGPwGxeN0no5fQsPA";
 
-//get city information with API => function
+//get weather information => function
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
 
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+//get city information with API => function
 const getCity = async (city) => {
   const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
   const query = `?apikey=${key}&q=${city}`;
@@ -13,6 +23,9 @@ const getCity = async (city) => {
 };
 
 getCity("Tbilisi")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
   .then((data) => {
     console.log(data);
   })
