4a171816860e4e869b35524f2c6316f0dc2512b7
Anahid <anahidhemmat2130@gmail.com>
2022-03-11 17:59:51 +0400

    setFormMessage

--- login&signup form/src/main.js
+++ login&signup form/src/main.js
@@ -1,3 +1,11 @@
+const setFormMessage = (formElement, type, message) => {
+  const messageElement = document.querySelector(".form-message");
+
+  messageElement.textContent = message;
+  messageElement.classList.remove("form-message-error", "form-message-success");
+  messageElement.classList.add(`form-message-${type}`);
+};
+
 //links
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
@@ -14,5 +22,9 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     loginForm.classList.remove("form-hidden");
     createAccountForm.classList.add("form-hidden");
+  });
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    setFormMessage(loginForm, "error", "Invalid username or password");
   });
 });
