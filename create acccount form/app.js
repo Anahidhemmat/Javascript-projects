53727cb9b6bf4360a76bd0cf961315c666e92c39
Anahid <anahidhemmat2130@gmail.com>
2022-03-12 21:12:03 +0400

    selectors

--- create acccount form/app.js
+++ create acccount form/app.js
@@ -1,1 +1,7 @@
-//
+//selectors
+
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordConfirm = document.getElementById("passwordConfirm");
