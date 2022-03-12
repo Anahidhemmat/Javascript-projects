13c84f36b46e6c72bad52222f263e196d117464b
Anahid <anahidhemmat2130@gmail.com>
2022-03-12 21:23:01 +0400

    get the values from inputs

--- create acccount form/app.js
+++ create acccount form/app.js
@@ -5,3 +5,20 @@ const username = document.getElementById("username");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const passwordConfirm = document.getElementById("passwordConfirm");
+
+//submition
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  checkInputs();
+});
+
+//check inputs function
+
+const checkInputs = () => {
+  // get the values from inputs
+  const usernameValue = username.value.trim();
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const passwordConfirmValue = passwordConfirm.value.trim();
+};
