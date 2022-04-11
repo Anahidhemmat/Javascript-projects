8607a676e5b913253f0b79f22f3ee29771c405d7
Anahid <anahidhemmat2130@gmail.com>
2022-04-11 22:03:05 +0400

    Update total cart

--- Shopping Cart/script.js
+++ Shopping Cart/script.js
@@ -5,5 +5,25 @@ for (let i = 0; i < removeCartItemBtns.length; i++) {
   btn.addEventListener("click", (e) => {
     let btnClicked = e.target;
     btnClicked.parentElement.parentElement.remove();
+    updateCartTotal();
   });
+}
+
+function updateCartTotal() {
+  const cartItemContainer = document.getElementsByClassName("cart-items")[0];
+  const cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  let total = 0;
+  for (let i = 0; i < cartRows.length; i++) {
+    const cartRow = cartRows[i];
+    const priceElement = cartRow.getElementsByClassName("cart-price")[0];
+    const quantityElement = cartRow.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+    const price = parseFloat(priceElement.innerText.replace("$", ""));
+    const quantity = quantityElement.value;
+
+    total = total + price * quantity;
+  }
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$" + total;
 }
