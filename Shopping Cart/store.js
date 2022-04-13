584442b28607f1d8720fbd8223af8c890378ddf3
Anahid <anahidhemmat2130@gmail.com>
2022-04-13 13:34:00 +0400

    adding items to cart

--- Shopping Cart/store.js
+++ Shopping Cart/store.js
@@ -6,17 +6,22 @@ if (document.readyState == "loading") {
 
 function ready() {
   const removeCartItemBtns = document.getElementsByClassName("btn-danger");
-
   for (let i = 0; i < removeCartItemBtns.length; i++) {
     let btn = removeCartItemBtns[i];
     btn.addEventListener("click", removeCartItem);
   }
-}
 
-const quantityInputs = document.getElementsByClassName("cart-quantity-input");
-for (let i = 0; i < quantityInputs.length; i++) {
-  const input = quantityInputs[i];
-  input.addEventListener("change", quantityChanged);
+  const quantityInputs = document.getElementsByClassName("cart-quantity-input");
+  for (let i = 0; i < quantityInputs.length; i++) {
+    const input = quantityInputs[i];
+    input.addEventListener("change", quantityChanged);
+  }
+
+  const addToCartBtns = document.getElementsByClassName("shop-item-button");
+  for (let i = 0; i < addToCartBtns.length; i++) {
+    const btn = addToCartBtns[i];
+    btn.addEventListener("click", addToCart);
+  }
 }
 
 function removeCartItem(e) {
@@ -33,6 +38,39 @@ function quantityChanged(e) {
   updateCartTotal();
 }
 
+function addToCart(e) {
+  const btn = e.target;
+  const shopItem = btn.parentElement.parentElement;
+  const title = shopItem.getElementsByClassName("shop-item-title")[0].innerText;
+  const price = shopItem.getElementsByClassName("shop-item-price")[0].innerText;
+  const imgSrc = shopItem.getElementsByClassName("shop-item-image")[0].src;
+  // console.log(title, price, imgSrc);
+  addItemToCart(title, price, imgSrc);
+}
+
+function addItemToCart(title, price, imgSrc) {
+  const cartRow = document.createElement("div");
+  cartRow.classList.add("cart-row");
+  const cartRowContents = `
+  <div class="cart-item cart-column">
+            <img
+              class="cart-item-image"
+              src="${imgSrc}"
+              width="100"
+              height="100"
+            />
+            <span class="cart-item-title">${title}</span>
+          </div>
+          <span class="cart-price cart-column">${price}</span>
+          <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="2" />
+            <button class="btn btn-danger" type="button">REMOVE</button>
+          </div>
+  `;
+  const cartItem = document.getElementsByClassName("cart-items")[0];
+  cartRow.innerHTML = cartRowContents;
+  cartItem.append(cartRow);
+}
 function updateCartTotal() {
   const cartItemContainer = document.getElementsByClassName("cart-items")[0];
   const cartRows = cartItemContainer.getElementsByClassName("cart-row");
